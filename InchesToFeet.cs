670c85e338bac24ab37982b83095fbd4558cbe44
spencerj <vorpehd@gmail.com>
2014-09-29 08:41:06 -0400

    ...
    ...

---
+++ InchesToFeet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
+class InchesToFeet
+{
+    static void Main(int inches = 45)
+    {
+    }
+
+    public static void InchesToFeet()
+    {
+
+    }
+}
